d2fa3a9a34b9cef1fa52005cc143d9de7b1498a4
dannyelcf <dannyelcf@gmail.com>
2018-10-24 10:24:31 -0300

    It was used string interpolation in server component as one way of data binding.

--- udemy/the-complete-guide-to-angular-2/my-first-app/src/app/server/server.component.ts
+++ udemy/the-complete-guide-to-angular-2/my-first-app/src/app/server/server.component.ts
@@ -5,5 +5,10 @@ import { Component } from "@angular/core";
     templateUrl: './server.component.html'
 })
 export class ServerComponent {
+    serverID: number = 10;
+    serverStatus: string = 'ofline';
 
+    getServerStatus(): string {
+        return this.serverStatus;
+    }
 }
