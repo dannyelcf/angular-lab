1889635f3330f85fea066ce3ebe9d796bef236b5
dannyelcf <dannyelcf@gmail.com>
2018-10-24 23:02:52 -0300

    It was used ngFor directive to create one app-server component element for each server in servers list.

--- udemy/the-complete-guide-to-angular-2/studies/my-first-app/src/app/servers/servers.component.ts
+++ udemy/the-complete-guide-to-angular-2/studies/my-first-app/src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ export class ServersComponent implements OnInit {
   serverCreationStatus = 'No server was created!';
   serverName = 'TestServer';
   serverCreated = false;
+  servers = ['Test Server 1', 'Test Server 2'];
 
   constructor() {
     setTimeout(() => {
@@ -27,6 +28,7 @@ export class ServersComponent implements OnInit {
 
   onCreateServer() {
     this.serverCreated = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = 'Server was created! Name is ' + this.serverName;
   }
 
