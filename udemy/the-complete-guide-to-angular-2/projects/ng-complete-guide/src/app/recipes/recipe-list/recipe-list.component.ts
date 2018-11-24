99f9a43ced6e95da12d0de273634a4218b12c6f6
dannyelcf <dannyelcf@gmail.com>
2018-11-24 20:45:24 -0200

    It was implemented recipe-list component.

--- udemy/the-complete-guide-to-angular-2/projects/ng-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
+++ udemy/the-complete-guide-to-angular-2/projects/ng-complete-guide/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://static.pourfemme.it/pfricette/fotogallery/pp/850x480/34345/carpaccio-di-zucchine-con-crostini.jpg'),
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://static.pourfemme.it/pfricette/fotogallery/pp/850x480/34345/carpaccio-di-zucchine-con-crostini.jpg')
+  ];
 
   constructor() { }
 
